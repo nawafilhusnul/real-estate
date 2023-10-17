4483a8f9c8205ff3237780f684895fe52f7963d4
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-17 21:11:04 +0700

    feat: connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('connected to db')
+}).catch((e)=>{
+  console.log('error connecting to db')
+});
 
 const app = express();
 
 app.listen(3000, ()=> {
   console.log('listening on port 3000!!!');
 })
+
+
