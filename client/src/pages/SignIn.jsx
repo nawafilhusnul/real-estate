c029978a3a573da25855c9e1e2a1f304e3aadf5e
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-17 20:23:09 +0700

    feat: add router using react router dom

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SignIn = (props) => {
+    return (
+        <div>
+        Sign In</div>
+    )
+};
+
+export default SignIn;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignUp(props) {
+    return (
+        <div>Sign Up</div>
+    );
+}
+
+export default SignUp;
