307040d96435ccb8b3f1894943ace4ba0390a96d
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-17 21:34:48 +0700

    feat: create a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -17,4 +18,4 @@ app.listen(3000, ()=> {
 })
 
 
-
+app.use('/api/user',userRouter)
