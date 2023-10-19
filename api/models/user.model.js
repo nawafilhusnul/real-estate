15799c4b12ed8c18f345eddf76f59f1bcb4cdacb
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-20 06:39:36 +0700

    feat: create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  avatar:{
+    type: String,
+    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fdefault-profile-picture&psig=AOvVaw0xRnipfl6N5mqXsckBLovk&ust=1697844246906000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCOi77uugg4IDFQAAAAAdAAAAABAE",
+  },
 }, {timestamps: true})
 
 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import {useDispatch, useSelector } from "react-redux";
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth.jsx";
 
 function SignIn(props) {
     const [ formData, setFromData ] = useState({});
@@ -34,7 +35,7 @@ function SignIn(props) {
                 return;
             }
 
-            dispatch(signInSuccess(data.data));
+            dispatch(signInSuccess(data));
             navigate('/');
         } catch (e) {
             dispatch(signInFailure(e.message));
@@ -50,6 +51,7 @@ function SignIn(props) {
                 <input type='text' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
                 <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
                 <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-60'>{loading ? 'Loading...' : 'Sign In'}</button>
+                <OAuth />
             </form>
             <div className='flex gap-2 mt-5'>
                 <p>Do not have an account?</p>
