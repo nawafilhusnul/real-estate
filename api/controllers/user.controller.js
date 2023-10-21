1bad00c3c205efb1ab969204bf03c3da2ad4e829
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-21 20:06:32 +0700

    feat: create delete user API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,3 +31,19 @@ export const updateUser = async (req, res, next)=> {
     next(e)
   }
 }
+
+export const deleteUser = async (req, res, next)=> {
+  if (req.user.id !== req.params.id) return next(errorHandler(403, 'You can only delete your own account'));
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({
+        message: 'Delete user successfully',
+        success: true,
+      })
+  } catch (e) {
+    next(e)
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
 
 userRouter.put('/:id', verifyToken, updateUser)
+userRouter.delete('/:id', verifyToken, deleteUser)
 
 export default userRouter;
