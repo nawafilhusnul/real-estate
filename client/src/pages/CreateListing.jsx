eafdd0cd7b3daec474361ba4daeff1fcc2b8c2ff
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-23 10:58:58 +0700

    fix: linter

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
 import { app } from "../firebase.js";
 import { useSelector } from "react-redux";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function CreateListing() {
     const navigate = useNavigate();
