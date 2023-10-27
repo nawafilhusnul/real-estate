01ad5915dae4421c666a111522190caaa0136f36
nawafilhusnul <nawafilhusnul@gmail.com>
2023-10-28 01:28:21 +0700

    feat: complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -22,7 +22,8 @@ function Profile() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [ updateSuccess, setUpdateSuccess ] = useState(false);
-
+    const [ showListingError, setshowListingError ] = useState(false);
+    const [ userListings, setUserListings ] = useState([]);
     useEffect(()=> {
         if (file) {
             handleFileUpload(file);
@@ -124,6 +125,21 @@ function Profile() {
         }
     }
 
+    const handleShowListings = async () => {
+        try {
+            setshowListingError(false);
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (!data.success) {
+                setshowListingError(true);
+                return;
+            }
+
+            setUserListings(data.data);
+        } catch (e) {
+            setshowListingError(true);
+        }
+    }
     return (
         <div
             className='p-3 max-w-lg mx-auto'>
@@ -220,6 +236,57 @@ function Profile() {
                     updateSuccess ? 'Successfully updated' : ''
                 }
             </p>
+            <button
+                onClick={handleShowListings}
+                className='text-green-700 w-full'>
+                Show Listing
+            </button>
+            <p
+                className='text-red-700'>
+                {
+                    showListingError
+                        ?
+                        'Error showing listings'
+                        :
+                        ''
+                }
+            </p>
+                {
+                    userListings && userListings.length > 0 &&
+                    <div
+                        className='flex flex-col gap-4'
+                    >
+                        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+                        {
+                            userListings.map((listing) => (
+                            <div
+                                key={listing._id}
+                                className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+                                <Link to={`/listing/${listing._id}`}>
+                                    <img
+                                        className='h-16 w-16 object-contain'
+                                        src={listing.imageUrls[0]}
+                                        alt={`listing ${listing._id}`}
+                                    />
+                                </Link>
+                                <Link
+                                    className='flex-1 text-slate-700 font-semibold hover:underline truncate'
+                                    to={`/listing/${listing._id}`}>
+                                    <p>
+                                        {listing.name}
+                                    </p>
+                                </Link>
+                                <div
+                                    className='flex flex-col items-center'
+                                >
+                                    <button className='text-red-700 uppercase'>Delete</button>
+                                    <button className='text-green-700 uppercase'>Edit</button>
+                                </div>
+                            </div>
+                            ))
+                        }
+                    </div>
+                }
         </div>
     );
 }
